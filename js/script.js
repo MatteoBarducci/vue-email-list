109fa8005378121506fc807a0bc9e0d6539ef66d
MatteoBarducci <mbardu1301@gmail.com>
2024-04-09 23:11:31 +0200

    add emails

--- js/script.js
+++ js/script.js
@@ -2,13 +2,22 @@ const { createApp } = Vue;
 
 createApp({
     data() {
-        
         return {
-            
+            emails: [],
         };
-        
     },
     methods: {
-        
-    }
+        generate10Emails() {
+            for (i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').
+                then((response) => {
+                    const NewEmail = response.data;
+                    this.emails.push(NewEmail.response);
+                });
+            }
+        },
+    },
+    mounted(){
+        this.generate10Emails();
+    },
 }).mount('#app');
